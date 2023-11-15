40a6ed5c1983f00314f4dee1cf97b3e9d722fbc8
Sabil667 <msabi@myuax.com>
2023-11-15 23:39:53 +0100

    .

--- EJ1/matematicas.cpp
+++ EJ1/matematicas.cpp
@@ -11,6 +11,11 @@ int main () {
     cin>> n2;
 
     double suma = n1+n2;
+    double resta = n1-n2;
+    double multiplicacion = n1*n2;
+    double division = n1/n2;
+    double potencia = pow(n1,n2);
+    double raiz = sqrt(n1);
 
 
 
