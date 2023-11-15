f8f0e088e8a9bc50dad476649ffcbcde45e54572
Sabil667 <msabi@myuax.com>
2023-11-15 23:39:10 +0100

    .

---
+++ EJ1/matematicas.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+    double n1,n2;
+    cout<< "Introduce un numero: ";
+    cin>> n1;
+    cout<< "Introduce otro numero: ";
+    cin>> n2;
+
+    double suma = n1+n2;
+
+
+
+
+}
