7514fa34a1c482f0b1ed92990531a2009a25ffe0
Sabil667 <msabi@myuax.com>
2023-11-16 00:36:03 +0100

    .

---
+++ EJ3/pentagono.cpp
@@ -0,0 +1,12 @@
+namespace Geometria{
+    class Pentagono{
+        private:
+            double lado;
+            double apotema;
+        public:
+            Pentagono(double lado, double apotema);
+            double area();
+    };
+
+
+}
