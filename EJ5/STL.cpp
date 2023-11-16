79cf322ceba0edb660ca107f23075a2fe548ba49
Sabil667 <msabi@myuax.com>
2023-11-16 01:50:01 +0100

    .

---
+++ EJ5/STL.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+
+
+int main(){
+
+    vector<int> numeros;
+    numeros.push_back(10);
+    numeros.push_back(20);
+    numeros.push_back(30);
+
+    cout<< "Vector: ";
+    for(const auto& num : numeros){
+        cout<< num<< " ";
+    }
+    cout<< "\n";
+
+    map<string, int> edad;
+
+    edad["Ghali"] = 25;
+    edad["Tobey Maguire"] = 30;
+    edad["Andrew Garfield"] = 22;
+
+
+
+}
